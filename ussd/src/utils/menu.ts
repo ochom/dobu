a264778805102a6f33af8f272d9d4f9c45b1da2d
ochom <ochomrichard752@gmail.com>
2022-08-24 18:30:56 +0300

    first ussd implementation

---
+++ ussd/src/utils/menu.ts
@@ -0,0 +1,10 @@
+import UssdMenu from "ussd-menu-builder";
+
+export default class MyMenu {
+  menu: UssdMenu;
+  constructor() {
+    this.menu = new UssdMenu();
+  }
+
+  // TODO build the menu here
+}
